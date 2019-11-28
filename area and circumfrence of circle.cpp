bac3efeefca16d3aac16b4539dada835cf06cc51
rahul263-stack <53581155+rahul263-stack@users.noreply.github.com>
2019-11-28 18:09:27 +0530

    e

---
+++ area and circumfrence of circle.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+	int r;
+	float pi=3.14,area,ci;
+	printf("enter radius of circle");
+	scanf("%d",&r);
+	area = pi*r*r;
+	printf("area of the circle=%f",ci);
+	ci=2*pi*r;
+	printf("circumfrence=%f",ci);
+}
