93d7637fb4d17ba28a6f465b8b185f9a185b7ba8
rahul263-stack <53581155+rahul263-stack@users.noreply.github.com>
2019-11-28 19:28:55 +0530

    wr

---
+++ program to illustrate without cpy.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+	char s1[50], s1[50]
+	int i ;
+	printf("enter string\n");
+	gets(s1);
+	for(i=0;s1[i]!='\0',i++)
+	{
+		s2[i]=s1[i]
+	}
+	s2[i]='\o';
+	printf("\n copied string is %d\n", s2);
+}
