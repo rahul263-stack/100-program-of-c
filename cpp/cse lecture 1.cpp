b70330285103d557167898e69cc187dcd26f70e5
rahul263-stack <53581155+rahul263-stack@users.noreply.github.com>
2020-01-08 12:52:24 +0530

    w

---
+++ cpp/cse lecture 1.cpp
@@ -0,0 +1,74 @@
+#include <iostream> //input output stream
+using namespace std;
+
+int main()
+{
+  //  int a = 12 , b= 34;
+    //cout<<"hello world"<<endl; //console output , <<insertion operator
+    //cout<<"i belong to india";// endl-- end the previous line
+    //cout<<"\n Value of a is"<<a;
+    //cout<<"\n value of b is"<<b;
+  //  int c,d,e;
+    //cout<<"Enter the value of c and d \n";
+    //cin>>c>>d; //cin console input  it is also the object of iostream symbol, >> extraction symbol -it extract value from user
+               //cin never use double cote
+      //         e = c+d;
+        //       cout<<"sum is "<<e;
+               //Write a program to input program and swappin of tow number
+          //     int a,b,temp;
+            //   cout<<"Enter the value of and b "<<endl;
+              // cin>>a>>b;
+             //temp = a;
+             //a=b;
+             //b=temp;
+               //cout<<"after swap a and b are "<<a<<"  "<<b;
+
+               //write a program to calculate SI by taking principal, rate , time as input from user
+  /*  float p,r,t,si;
+   cout<<"Enter the value of p,r and t\n";
+   cin>>p>>r>>t;
+   si=(p*r*t)/100;
+           cout<<"Simple intrest is  "<<si; */
+
+  //write a program to take input of five variable and calculate its average
+  /*    int a,b,c,d,e,avg;
+      cout<<"Enter the values of a,b,c,d,e \n";
+      cin>>a>>b>>c>>d>>e;
+      avg= (a+b+c+d+e)/5;
+              cout<<"avg of the given number is "<<avg; */
+
+  // write a program to take input of two variables and find out greater of them
+  // write a program to input three varibles and find out thee greatest of them
+
+    /*  int a,b;
+      cout<<"Enter the value of a and b\n";
+cin>>a>>b;
+      if(a>b)
+          cout<<"a is greater \n";
+      else
+          cout<<"b is greater";*/
+
+  /*  int a,b,c;
+    cout<<"enter the value of a,b,c/n";
+    cin>>a>>b>>c;
+    if(a>b && a>c)
+        cout<<"a is the greatest number\n";
+    else if (b>c)
+        cout<<"b os greatest ";
+    else
+        cout<<"c is greatest"; */
+
+  //write a program to check if aa number is positive negative or zero
+
+  /*int a;
+  cout<<"'enter the value of a /n";
+  if(a>0)
+      cout<<"a is positive";
+  else if (a<0)
+      cout<<"a is negative";
+  else
+      cout<<"a is zero "; */
+
+
+
+}
